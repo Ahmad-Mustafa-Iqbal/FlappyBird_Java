97b39d11b840769f5372c6ceac4bd4ba9483a738
Ahmad Mustafa Iqbal <ahmadmustafaand@example.com>
2025-02-18 23:14:25 +0500

    paint Component and draw function and images are added

--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -8,9 +8,53 @@ public class FlappyBird extends JPanel{
     int boardHeight=640;
     int boardWidth=360;
 
+    Image backgroundImg;
+    Image birdImg;
+    Image TopPipeImg;
+    Image BottomPipeImg;
+
+    int birdX = boardWidth/8;
+    int birdY = boardHeight/2;
+    int birdWidth = 34;
+    int birdHeight = 24;
+
+    class Bird{
+        int x = birdX;
+        int y = birdY;
+        int width = birdWidth;
+        int height = birdHeight;
+        Image img;
+
+        Bird(Image img){
+            this.img = img;
+        }
+    }
+
+    Bird bird;
+
+
+
     FlappyBird(){
         setPreferredSize(new Dimension(boardWidth, boardHeight));
-        setBackground(Color.blue);
+        // setBackground(Color.blue);
+
+        backgroundImg = new ImageIcon(getClass().getResource("./flappybirdbg.png")).getImage();
+        birdImg = new ImageIcon(getClass().getResource("./flappybird.png")).getImage();
+        TopPipeImg= new ImageIcon(getClass().getResource("./topPipe.png")).getImage();
+        BottomPipeImg = new ImageIcon(getClass().getResource("bottompipe.png")).getImage();
+
+        bird = new Bird(birdImg);
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g){
+      g.drawImage(backgroundImg, 0, 0, boardWidth, boardHeight, null);
+
+      g.drawImage(bird.img, bird.x, bird.y, bird.width, bird.height, null);
     }
     
 }
