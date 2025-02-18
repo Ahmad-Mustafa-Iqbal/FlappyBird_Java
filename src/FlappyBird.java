519206c5b21472e088ef5b8473a7b8976f7d805a
Ahmad Mustafa Iqbal <ahmadmustafaand@example.com>
2025-02-18 22:33:20 +0500

    JPanel And JFrame is Added for Game.

---
+++ src/FlappyBird.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class FlappyBird extends JPanel{
+    int boardHeight=640;
+    int boardWidth=360;
+
+    FlappyBird(){
+        setPreferredSize(new Dimension(boardWidth, boardHeight));
+        setBackground(Color.blue);
+    }
+    
+}
