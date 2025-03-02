f875a1059b56a4cbe4eb932ff6a8ff34a59fae20
Ahmad Mustafa Iqbal <ahmadmustafaand@example.com>
2025-03-02 12:45:41 +0500

    Nothing

--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -9,13 +9,12 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener {
     int boardWidth = 360;
     int boardHeight = 640;
 
-    //images
+
     Image backgroundImg;
     Image birdImg;
     Image topPipeImg;
     Image bottomPipeImg;
 
-    //bird class
     int birdX = boardWidth/8;
     int birdY = boardWidth/2;
     int birdWidth = 34;
@@ -33,10 +32,9 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener {
         }
     }
 
-    //pipe class
     int pipeX = boardWidth;
     int pipeY = 0;
-    int pipeWidth = 64;  //scaled by 1/6
+    int pipeWidth = 64;  
     int pipeHeight = 512;
     
     class Pipe {
